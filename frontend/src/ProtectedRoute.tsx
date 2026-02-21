8714968a25af4bd3629c37312121846335b264c0
Trixie Vicgor Limanto <z5465113@ad.unsw.edu.au>
2026-02-21 14:32:20 +1100

    Front end setup done

---
+++ frontend/src/ProtectedRoute.tsx
@@ -0,0 +1,25 @@
+import { Navigate } from 'react-router-dom'
+import React from 'react';
+import useAuthStore from './stores/authStore';
+
+type ProtectedRouteProp = {
+  children: React.ReactElement;
+  auth?: boolean;
+}
+
+const ProtectedRoute = ({ children, auth = false }: ProtectedRouteProp) => {
+  const { isAuthenticated, token } = useAuthStore();
+  const hasToken = !!token;
+
+  if (!auth && !hasToken && !isAuthenticated) {
+    return <Navigate to='/' />;
+  }
+
+  if (auth && hasToken && isAuthenticated) {
+    return <Navigate to='/dashboard' />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
