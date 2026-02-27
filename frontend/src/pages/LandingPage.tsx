24462d494c9da93446fd1830c8bab4162db5ea5a
Trixie Vicgor Limanto <z5465113@ad.unsw.edu.au>
2026-02-27 17:43:03 +1100

    Prepare for Cloudflare Pages deployment

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,5 @@
-import { useNavigate } from 'react-router-dom';
-import { RiAccountCircle2Line } from "react-icons/ri";
+// import { useNavigate } from 'react-router-dom';
+import { RiAccountCircle2Line } from 'react-icons/ri';
 import Layout from '../components/Layout';
 import { useGSAP } from '@gsap/react';
 import { useRef } from 'react';
@@ -8,43 +8,44 @@ import gsap from 'gsap';
 gsap.registerPlugin(useGSAP);
 
 const LandingPage = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const carouselRef1 = useRef<HTMLDivElement>(null);
   const carouselRef2 = useRef<HTMLDivElement>(null);
+  const bigMoversSelection = ['Portfolio', 'Stock', 'Crypto', 'ETF'];
   const highlightedMarkets = [
-    { name: 'GOLD', price: '$1,800.00', change: '+0.5%' },
-    { name: 'GOLD', price: '$1.00', change: '+0.5%' },
-    { name: 'SHIBALLLL', price: '$1,800.00', change: '+0.5%' },
-    { name: 'SHIBALLLL', price: '$1,800.00', change: '+0.5%' },
-    { name: 'SHIBALLLL', price: '$1,800.00', change: '+0.5%' },
-    { name: 'BHAHBSHBKSABHKAS', price: '$1,800.00', change: '+0.5%' },
-    { name: 'BHAHBSHBKSABHKAS', price: '$1,800.00', change: '+0.5%' },
-    { name: 'BHAHBSHBKSABHKAS', price: '$1,800.00', change: '+0.5%' },
-    { name: 'BHAHBSHBKSABHKAS', price: '$1,800.00', change: '+0.5%' },
-    { name: 'BHAHBSHBKSABHKAS', price: '$1,800.00', change: '+0.5%' },
+    { name: 'GLD', price: '$1,800.00', change: '+0.5%' },
+    { name: 'BTC', price: '$1.00', change: '+0.5%' },
+    { name: 'NVDA', price: '$1,800.00', change: '+0.5%' },
+    { name: 'MSFT', price: '$1,800.00', change: '+0.5%' },
+    { name: 'SPY', price: '$1,800.00', change: '+0.5%' },
+    { name: 'QQQ', price: '$1,800.00', change: '+0.5%' },
+    { name: 'BND', price: '$1,800.00', change: '+0.5%' },
+    { name: 'AAPL', price: '$1,800.00', change: '+0.5%' },
+    { name: 'N225', price: '$1,800.00', change: '+0.5%' },
+    { name: 'DIA', price: '$1,800.00', change: '+0.5%' },
   ];
 
   useGSAP(() => {
     gsap.fromTo(
       carouselRef1.current,
       { xPercent: 0 },
-      { xPercent: -100, duration: 10, ease: 'none', repeat: -1 }
+      { xPercent: -100, duration: 30, ease: 'none', repeat: -1 }
     );
     gsap.fromTo(
       carouselRef2.current,
       { xPercent: 0 },
-      { xPercent: -100, duration: 10, ease: 'none', repeat: -1 }
+      { xPercent: -100, duration: 30, ease: 'none', repeat: -1 }
     );
   });
   
 
   const headerButtons = (
-    <div className='flex items-center gap-4'>
+    <div className='flex items-center justify-center gap-4 w-13 h-13'>
       <button
         // onClick={() => navigate('/login')}
-        className='text-white px-1 py-1 rounded-xl hover:bg-[#3d4243] font-bold'
+        className='text-white font-normal px-1 py-1'
       >
-        <RiAccountCircle2Line className="h-15 w-15"/>
+        <RiAccountCircle2Line className='h-12 w-12 hover:h-13 hover:w-13'/>
       </button>
     </div>
   );
@@ -52,9 +53,9 @@ const LandingPage = () => {
   return (
     <Layout headerRight={headerButtons}>
       {/* Main Content */}
-      <section className='container flex flex-col max-w-screen flex-grow py-3'>
+      <section className='container flex flex-col max-w-screen flex-grow divide-y-2 divide-[#3d4243]'>
         {/* Market Highlight */}
-        <div className='flex width-100vw overflow-hidden'>
+        <div className='flex width-100vw overflow-hidden py-3'>
           <div className='flex pr-4 gap-4' ref={carouselRef1}>
             {highlightedMarkets.map((market, index) => (
               <div key={index} className='flex flex-col w-40 h-20 items-start justify-center pr-4 border-r-2 border-[#3d4243]'>
@@ -89,6 +90,39 @@ const LandingPage = () => {
               </div>
             ))}
           </div>
+        </div>
+        {/* Today's Big Movers, Top Gainers & Top Losers */}
+        <div className='flex flex-col p-3'>
+          <div className='flex divide-x-2 divide-[#3d4243]'>
+            {bigMoversSelection.map((title, index) => (
+              <div key={index} className='flex px-3'>
+                <button
+                  className='w-25 py-1 text-white text-lg hover:font-bold hover:bg-[#2a2e30] hover:rounded-xl transition-all'
+                >
+                  {title}
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className='flex divide-x-2 divide-[#3d4243] gap-4 px-3 py-3'>
+            <div className='flex w-1/2 justify-left'>
+              <h1 className='text-white font-bold text-4xl'>
+                Top Gainers
+              </h1>
+            </div>
+
+            <div className='flex w-1/2 justify-left'>
+              <h1 className='text-white font-bold text-4xl'>
+                Top Losers
+              </h1>
+            </div>
+          </div>
+        </div>
+        
+        <div className='flex py-3'>
+          <h1 className='text-white '>
+            hahaha
+          </h1>
         </div>
       </section>
     </Layout>
