996f2406bcc9b4962d25d89323f76c651ccfdb94
Trixie Vicgor Limanto <z5465113@ad.unsw.edu.au>
2026-02-28 19:51:05 +1100

    Remodel highlights and started Daily Movers

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
+
 interface LayoutProps {
   children: React.ReactNode;
   headerRight?: React.ReactNode;
 }
 
 const Layout = ({ children, headerRight }: LayoutProps) => {
-  const maintenence = true;
+  const password = 'trixie';
+  const [maintenence, setMaintenence] = useState(true);
+  const [input, setInput] = useState('');
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
 
+    if (input === password) {
+      setMaintenence(false);
+    }
+  };
   return (
     <div className='relative min-h-screen overflow-hidden bg-black'>
       {/* Maintenence Notice */}
@@ -14,11 +24,20 @@ const Layout = ({ children, headerRight }: LayoutProps) => {
           BlackVaultz
         </p>
         <p className='font-semibold text-5xl block'>
-          Web is currently under construction
+          BlackVaultz is Currently Available by Invitation Only
         </p>
         <p className='font-semibold text-2xl block mt-4'>
-          Estimated launch: 01 January 2027
+          Public Access Will Be Available On 1st January 2027
         </p>
+        <form onSubmit={handleSubmit} className='mt-8'>
+          <input
+            type='password'
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className='px-4 py-2 rounded-md bg-[#3d4243]/20 border-2 border-transparent hover:bg-[#3d4243]/90 hover:border-white transition-all'
+            placeholder='Enter password'
+          />
+        </form>
       </div>}
 
       {/* Main Page */}
--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -2,19 +2,23 @@
 import { RiAccountCircle2Line } from 'react-icons/ri';
 import Layout from '../components/Layout';
 import { useGSAP } from '@gsap/react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
+import { IoTrendingDown, IoTrendingUp } from 'react-icons/io5';
 
 gsap.registerPlugin(useGSAP);
 
 const LandingPage = () => {
   // const navigate = useNavigate();
+  const [dailyMovers, setDailyMovers] = useState(true);
   const carouselRef1 = useRef<HTMLDivElement>(null);
   const carouselRef2 = useRef<HTMLDivElement>(null);
+  const carouselRef3 = useRef<HTMLDivElement>(null);
+  const carouselRefs = [carouselRef1, carouselRef2, carouselRef3];
   const bigMoversSelection = ['Portfolio', 'Stock', 'Crypto', 'ETF'];
   const highlightedMarkets = [
     { name: 'GLD', price: '$1,800.00', change: '+0.5%' },
-    { name: 'BTC', price: '$1.00', change: '+0.5%' },
+    { name: 'BTC', price: '$180,241.00', change: '+0.5%' },
     { name: 'NVDA', price: '$1,80.00', change: '+0.5%' },
     { name: 'MSFT', price: '$1,800.00', change: '+0.5%' },
     { name: 'SPY', price: '$1,800.00', change: '+0.5%' },
@@ -28,24 +32,20 @@ const LandingPage = () => {
   useEffect(() => {
     const favicon = document.querySelector("link[rel='icon']") as HTMLLinkElement;
     if (favicon) {
-      favicon.href = "/Logo.png";
+      favicon.href = '/Logo.png';
     }
-    document.title = "BlackVaultz";
+    document.title = 'BlackVaultz';
   }, []);
 
   useGSAP(() => {
-    gsap.fromTo(
-      carouselRef1.current,
-      { xPercent: 0 },
-      { xPercent: -100, duration: 30, ease: 'none', repeat: -1 }
-    );
-    gsap.fromTo(
-      carouselRef2.current,
-      { xPercent: 0 },
-      { xPercent: -100, duration: 30, ease: 'none', repeat: -1 }
-    );
+    for (let i = 0; i < 3; i++) {
+      gsap.fromTo(
+        carouselRefs[i].current,
+        { xPercent: 0 },
+        { xPercent: -100, duration: 30, ease: 'none', repeat: -1 }
+      );
+    }
   });
-  
 
   const headerButtons = (
     <div className='flex items-center justify-center gap-4 w-13 h-13'>
@@ -64,72 +64,77 @@ const LandingPage = () => {
       <section className='container flex flex-col max-w-screen flex-grow divide-y-2 divide-[#3d4243]'>
         {/* Market Highlight */}
         <div className='flex width-100vw overflow-hidden py-3'>
-          <div className='flex pr-4 gap-4' ref={carouselRef1}>
-            {highlightedMarkets.map((market, index) => (
-              <div key={index} className='flex flex-col w-40 h-20 items-start justify-center pr-4 border-r-2 border-[#3d4243]'>
-                <h1 className='text-xs font-bold text-white'>
-                  {market.name}
-                </h1>
-                <h1 className='text-lg font-bold text-white'>
-                  {market.price}
-                </h1>
-                <h1 className={`text-xs ${
-                  market.change.startsWith('+') ? 'text-green-500' : 'text-red-500'
-                }`}>
-                  {market.change}
-                </h1>
-              </div>
-            ))}
-          </div>
-          <div className='flex pr-4 gap-4' ref={carouselRef2}>
-            {highlightedMarkets.map((market, index) => (
-              <div key={index} className='flex flex-col w-40 h-20 items-start justify-center pr-4 border-r-2 border-[#3d4243]'>
-                <h1 className='text-xs font-bold text-white'>
-                  {market.name}
-                </h1>
-                <h1 className='text-lg font-bold text-white'>
-                  {market.price}
-                </h1>
-                <h1 className={`text-xs ${
-                  market.change.startsWith('+') ? 'text-green-500' : 'text-red-500'
-                }`}>
-                  {market.change}
-                </h1>
-              </div>
-            ))}
-          </div>
+          {carouselRefs.map((ref, i) => (
+            <div className='flex pr-2 gap-2' ref={ref} key={i}>
+              {highlightedMarkets.map((market, index) => (
+                <div key={index} className='flex flex-row text-sm font-bold h-5 items-center justify-center gap-2 pr-4 border-r-2 border-[#3d4243]'>
+                  <h1 className='text-white'>
+                    {market.name}
+                  </h1>
+                  <h1 className='text-white'>
+                    {market.price}
+                  </h1>
+                  <h1 className={`${
+                    market.change.startsWith('+') ? 'text-green-500' : 'text-red-500'
+                  }`}>
+                    {market.change}
+                  </h1>
+                </div>
+              ))}
+            </div>
+          ))}
         </div>
-        {/* Today's Big Movers, Top Gainers & Top Losers */}
-        <div className='flex flex-col p-3'>
-          <div className='flex divide-x-2 divide-[#3d4243]'>
-            {bigMoversSelection.map((title, index) => (
-              <div key={index} className='flex px-3'>
+        <div className='flex p-3 divide-x-2 divide-[#3d4243] gap-4'>
+          {/* Today's Big Movers */}
+          <div className='px-3 flex flex-col w-1/2 justify-left'>
+            <h1 className='font-bold text-4xl'>Daily Movers</h1>
+            <div className='flex flex-row items-center justify-between pt-3'>
+              <h1 className='py-3 font-bold text-3xl'>{dailyMovers ? 'Top Bulls' : 'Top Bears'}</h1>
+              <div className='flex gap-2 transition-all text-4xl'>
                 <button
-                  className='w-25 py-1 text-white text-lg hover:font-bold hover:bg-[#2a2e30] hover:rounded-xl transition-all'
+                  onClick={() => setDailyMovers(false)}
+                  className={`${!dailyMovers ? 'opacity-100' : 'opacity-50'} hover:opacity-100 hover:scale-110`}
                 >
-                  {title}
+                  <IoTrendingDown className='text-red-500' />
+                </button>
+                <button
+                  onClick={() => setDailyMovers(true)}
+                  className={`${dailyMovers ? 'opacity-100' : 'opacity-50'} hover:opacity-100 hover:scale-110`}
+                >
+                  <IoTrendingUp className='text-green-500' />
                 </button>
               </div>
-            ))}
+            </div>
+              <div className='flex divide-x-2 divide-[#3d4243]'>
+                {bigMoversSelection.map((title, index) => (
+                  <div key={index} className='flex px-1'>
+                    <button
+                      className='w-25 py-1 text-white text-sm hover:font-bold hover:bg-[#2a2e30] hover:rounded-xl transition-all'
+                    >
+                      {title}
+                    </button>
+                  </div>
+                ))}
+              </div>
+              <div className='py-5'>
+                NVDA
+              </div>
           </div>
-          <div className='flex divide-x-2 divide-[#3d4243] gap-4 px-3 py-3'>
-            <div className='flex w-1/2 justify-left'>
-              <h1 className='text-white font-bold text-4xl'>
-                Top Gainers
-              </h1>
-            </div>
-
-            <div className='flex w-1/2 justify-left'>
-              <h1 className='text-white font-bold text-4xl'>
-                Top Losers
-              </h1>
-            </div>
+          {/* Today's News */}
+          <div className='flex justify-left'>
+            <h1 className='text-white font-bold text-4xl'>
+              Today's News
+            </h1>
           </div>
         </div>
         
-        <div className='flex py-3'>
-          <h1 className='text-white '>
-            hahaha
+        {/* Analysis */}
+        <div className='flex p-6'>
+          <h1 className='text-white font-bold text-4xl'>
+            Trending Analysis
+          </h1>
+          <h1 className='text-white font-bold text-2xl'>
+            Overview
           </h1>
         </div>
       </section>
